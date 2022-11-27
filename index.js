754a306994b9d0889be3197e33af26153256249f
Mary <116301135+fraumarzhuk@users.noreply.github.com>
2022-11-27 19:11:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+//
+//
+// Detecting button press
+
+var numberofDrumButtons  = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberofDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+   var buttoninnerHTML = this.innerHTML;
+  makesound(buttoninnerHTML);
+});
+}
+
+// Detection keyboard press
+
+document.addEventListener("keydown", function(event){
+     makesound(event.key);
+});
+
+function makesound(key) {
+
+      switch (key) {
+        case "w":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+          break;
+
+        case "a":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+          break;
+
+        case "s":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+          break;
+
+        case "d":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+          break;
+
+        case "j":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+          break;
+
+        case "k":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+          break;
+
+          case "l":
+          var tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+            break;
+
+        default: console.log(buttoninnerHTML);
+      }
+}
+
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
